3121382d0b93e6447082549ce147e89359bb28b4
Ian Cloutier <ian@vulcanforms.com>
2020-09-24 17:13:55 -0400

    initial files

---
+++ LUTConv.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 22 12:55:19 2020
+
+@author: ian
+"""
+
+
+import numpy as np
+import pandas as pd
+import os
+import glob2
+import zlib
+
+nameprefix='VF-LaserPowerLUT_'
+
+
+def Convert_LUT_to_vflpc(csvpath,binpath):
+    os.chdir(csvpath)
+    print('Starting Conversion')
+    for csv in glob2.glob(csvpath+'*/*.csv'):
+        data=pd.read_csv(csv, dtype = np.uint16, header=None)
+    filebinname=csv[-19:-4]+'.vflpc'
+    racknumber=filebinname[0:3]
+    lasernumber=filebinname[4:6]
+    fullpath=binpath+racknumber+'/'+nameprefix+racknumber+'_P'+lasernumber+'.vflpc'
+    bites=np.asarray(data.values).tobytes()
+    crccode=zlib.crc32(bites)
+    ba=bytearray(crccode.to_bytes(4, 'little'))
+    crcbites=np.asarray(ba).tobytes()
+    final=bites+crcbites
+    #print(final)
+    np.asarray(final).tofile(fullpath)
+    print('Conversion done')
---
+++ LUTUtility.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 24 13:51:31 2020
+
+@author: ian
+"""
+
+
+from BNRopcuaTag import BNRopcuaTag
+from ftplib import FTP
+from LUTConv import Convert_LUT_to_vflpc
+import os
+import glob2
+csvpath='C:/LaserLUT/LUTcsvFiles/'
+binpath='C:/LaserLUT/LUTbinaryFiles/'
+IPs=['192.168.210.51','192.168.210.52', '192.168.210.53', '192.168.210.54', '192.168.210.55', '192.168.210.56', '192.168.210.57', '192.168.210.58' ]
+os.chdir(binpath)
+Convert_LUT_to_vflpc(csvpath,binpath)
+for R in range(1,8):
+    ftp = FTP(IPs[R-1])
+    ftp.login(user='admin', passwd='VFftp')
+    ftp.cwd('/media/ssd1')
+    rackstring='R'+str(R).zfill(2)
+    searchscheme='*/*'+rackstring+'*.vflpc'
+    #print(binpath+searchscheme)
+    for fi in glob2.glob(binpath+searchscheme):
+        #print(fi)
+        filename = fi
+        #ftp.storbinary('STOR '+filename, open(filename, 'rb'))
+        
+    #ftp.quit()
+    
