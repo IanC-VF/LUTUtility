4abce62ca186da9dd54c97e1ab96ae9a4c16d2b2
IanC-VF <64868217+IanC-VF@users.noreply.github.com>
2021-04-22 15:20:30 -0400

    edited bat file

--- GenerateLUTfiles.bat
+++ GenerateLUTfiles.bat
@@ -1,3 +1,3 @@
 call C:\ProgramData\Miniconda3\Scripts\activate.bat
-"C:\ProgramData\Miniconda3\python.exe" "C:\LUTUtility\Generate_vflpc_from_Coefficients.py"
+"C:\ProgramData\Miniconda3\python.exe" "C:\LUTUtility\ParameterizedLUTGen.py"
 pause
