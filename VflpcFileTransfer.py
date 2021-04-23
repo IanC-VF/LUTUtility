06bbbc507a36815f31e0a7a70a9a9e434802c1a8
Ian <ian@vulcanforms.com>
2021-04-23 10:47:34 -0400

    file transfer script updated for gen2

--- VflpcFileTransfer.py
+++ VflpcFileTransfer.py
@@ -9,19 +9,18 @@ Created on Thu Nov 19 15:55:23 2020
 from ftplib import FTP
 import os
 import glob2
-csvpath='C:/LaserLUTfiles/LUTcsvFiles/'
-binpath='C:/LaserLUTfiles/LUTbinaryFiles/'
-IPs=['192.168.210.51','192.168.210.52', '192.168.210.53', '192.168.210.54', '192.168.210.55', '192.168.210.56', '192.168.210.57', '192.168.210.58' ]
+binpath='C:/LUTDataGeneration/BIN/'
+IPs=['192.168.210.51','192.168.210.52', '192.168.210.53', '192.168.210.54', '192.168.210.55', '192.168.210.56', '192.168.210.57' ]
 os.chdir(binpath)
-for R in range(7,8):
+for R in range(1,8):
     ftp = FTP(IPs[R-1])
     ftp.login(user='admin', passwd='VFftp')
     ftp.cwd('/MachineParameters')
     rackstring='R'+str(R).zfill(2)
     searchscheme='*/*'+rackstring+'*.vflpc'
-    #print(binpath+searchscheme)
+    print(binpath+searchscheme)
     for fi in glob2.glob(binpath+searchscheme):
-        filename = fi[36:]
+        filename = fi[29:]
         ftp.storbinary('STOR '+filename, open(fi, 'rb'))
-        print('File Transferred:'+fi[36:])
+        print('File Transferred:'+fi[29:])
     ftp.quit()
